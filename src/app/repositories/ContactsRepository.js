2d5e35fe6b5f22fd511612f00140ab4b023f3af3
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-07-16 10:01:05 -0300

    refactoring delete method - CRUD refactored

--- src/app/repositories/ContactsRepository.js
+++ src/app/repositories/ContactsRepository.js
@@ -1,23 +1,4 @@
-const { v4 } = require('uuid');
-
 const db = require('../../database/index');
-
-let contacts = [
-  {
-    id: v4(),
-    name: 'Anderson',
-    email: 'anderson@mail.com',
-    phone: '1199999999',
-    category_id: v4(),
-  },
-  {
-    id: v4(),
-    name: 'Jose',
-    email: 'jose@mail.com',
-    phone: '1199999999',
-    category_id: v4(),
-  },
-];
 
 class ContactsRepository {
   async findAll(orderBy = 'ASC') {
@@ -63,11 +44,9 @@ class ContactsRepository {
     return row;
   }
 
-  delete(id) {
-    return new Promise((resolve) => {
-      contacts = contacts.filter((contact) => contact.id != id);
-      resolve();
-    });
+  async delete(id) {
+    const deleteOp = await db.query('DELETE FROM contacts WHERE id = $1', [id]);
+    return deleteOp;
   }
 }
 
