b7fcb2cd75f17a966b3c6e895fbafd5fd8c6d4c3
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-07-16 12:37:25 -0300

    created update and delete method for categories module

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -17,6 +17,39 @@ class CategoryController {
 
     response.json(category);
   }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name } = request.body;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      return response.status(404).json({ error: 'Category not found' });
+    }
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const category = await CategoriesRepository.update(id, name);
+
+    response.json(category);
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const categoryExists = await CategoriesRepository.findById(id);
+
+    if (!categoryExists) {
+      //404: Not found
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await CategoriesRepository.delete(id);
+    // 204: No content
+    response.sendStatus(204);
+  }
 }
 
 module.exports = new CategoryController();
--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,13 @@ const router = Router();
 
 router.get('/contacts', contactController.index);
 router.get('/contacts/:id', contactController.show);
-router.delete('/contacts/:id', contactController.delete);
 router.post('/contacts', contactController.store);
+router.delete('/contacts/:id', contactController.delete);
 router.put('/contacts/:id', contactController.update);
 
 router.get('/categories', CategoryController.index);
 router.post('/categories', CategoryController.store);
+router.delete('/categories/:id', CategoryController.delete);
+router.put('/categories/:id', CategoryController.update);
 
 module.exports = router;
