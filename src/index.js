3e9207b5e38a8e8dda371ad54f506e2c2060b8cb
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-07-16 11:35:09 -0300

    error handler and express-async-errors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+require('express-async-errors');
 
 app.use(express.json());
 app.use(routes);
+//Error Handler (Middleware express) --> Após definição das rotas.
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
